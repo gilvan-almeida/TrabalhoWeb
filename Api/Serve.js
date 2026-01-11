5fa3335f52cecb1c8003acc7b8cd76c73b861638
unknown <gilvanalmeidaaraujo748@gmail.com>
2026-01-11 18:35:43 -0300

    feat: controllers Api e Back

--- Api/Serve.js
+++ Api/Serve.js
@@ -2,6 +2,6 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando na porta ${PORT}`);
-    console.log(`🔗 http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
