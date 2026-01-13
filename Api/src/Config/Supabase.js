65d4792bc26d5e36bc61d876fcfa9d87a0690a8c
unknown <gilvanalmeidaaraujo748@gmail.com>
2026-01-12 23:02:39 -0300

    feat: supabase

--- Api/src/Config/Supabase.js
+++ Api/src/Config/Supabase.js
@@ -4,6 +4,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
+//teste
 console.log("URL do Supabase carregada:", supabaseUrl ? "SIM" : "NÃO");
 console.log("Key do Supabase carregada:", supabaseKey ? "SIM" : "NÃO");
 
